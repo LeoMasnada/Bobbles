7ec68c8617752e2ceaab03e74a9863bed66d794b
Leo Masnada <leo-mas-@hotmail.com>
2020-07-22 00:28:03 +0200

    Update off.js

--- commands/off.js
+++ commands/off.js
@@ -1,8 +1,6 @@
-const config = require('./../data/botconfig.json');
-
 module.exports.run = async (bot, message) => {
 	// if the user is not the developper
-	if (!message.author.id == '250547618537537537') return message.reply('Nani !? You don\'t fill the requirement for this !');
+	if (!message.author.id == '250547618537537537') return ;
 
 	// reboots
 	await message.channel.send('Turning off');
@@ -12,5 +10,4 @@ module.exports.run = async (bot, message) => {
 module.exports.help = {
 	name: 'off',
 	description: 'Stops the bot. Only available for bot owner',
-	usage: `${config.prefix}off`,
 };
