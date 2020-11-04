5051f75e323f764aac6a13b8d9302b19b5fa3ed7
Leo Masnada <leo-mas-@hotmail.com>
2020-11-04 14:34:20 +0100

    roll

---
+++ commands/roll.js
@@ -0,0 +1,44 @@
+module.exports = {
+	name: 'roll',
+	description: 'Jet de dés',
+	alias: ['r'],
+	async execute(message, args) {
+
+		// Checks validity of the command
+		if (!args[0]) return message.reply('Precisez le nombre et type de des a lancer "XdY"');
+		// Initializes the number and type of dice from the command called
+		const x = parseInt(args[0].split('d')[0]);
+		const y = parseInt(args[0].split('d')[1]);
+
+		// If x and y are valid numbers
+		if (x && y) {
+			// Sends confirmation message
+			message.channel.send(`Lancement de ${x}d${y}...`)
+			// Then logs all the dices thrown and each value resulting
+				.then(() =>{
+					// Init of storage values
+					const results = new Array();
+					let total = 0;
+					// For each die log its value in number and string form
+					for (let i = 0; i < x; i++) {
+						const nb = Math.round(Math.random() * y);
+						results.push(nb);
+						total += nb;
+					}
+					// Init final message string
+					let str = '';
+					// For each die, add to the string its value
+					for (let i = 0; i < results.length - 1; i++) {
+						str += results[i] + '+';
+					}
+					str += results[results.length - 1];
+					// Sends out result
+					message.channel.send('Resultat: ' + str + '\nTotal: ' + total);
+				});
+		}
+		// If x or y was invalid, abort
+		else{
+			message.reply('Precisez le nombre et type de des a lancer "XdY"');
+		}
+	},
+};
